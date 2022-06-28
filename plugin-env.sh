078d7107135c9c4aa36cb806642c785d192f152e
Wasseem Khayrattee <w@7php.com>
2022-06-29 00:24:41 +0400

    feat: add an env file which I or any other user can adjust custom values for the creation of a new plugin
    This is the only file that a user is expected to touch. The rest would pretty much be on autopilot.

---
+++ plugin-env.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+##
+### A central place to add all reusable stuffs
+### @author Wasseem Khayrattee <github@wk.contact>
+### @github wkhayrattee
+##
+
+AUTHOR_NAME="Wasseem Khayrattee"
+AUTHOR_WP_ORG_USERNAME="wkhayrattee"
+AUTHOR_EMAIL="github@wk.contact"
+AUTHOR_URI="https://github.com/wkhayrattee"
+### should be in a valid slug
+PROJECT_SLUG="wp-plugin-starter"
+PROJECT_NAME="WP Plugin Starter"
+PROJECT_GITHUB="todo"
+#
+COMPOSER_NAMESPACE="wkhayrattee"
+## Should be CamelCase
+PSR_AUTOLOAD_NAMESPACE="WpPluginStarter"
+PHP_CONSTANT_PREFIX="WP_PLUGIN_STARTER"
+## to house handy procedural functions
+HELPER_FILE_NAME="${PROJECT_SLUG}-plugin-helper"
+#
+LICENCE="GPL-3.0-only"
+LICENCE_URI="https://www.gnu.org/licenses/gpl-3.0.html"
+
