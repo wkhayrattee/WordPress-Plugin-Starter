40642923d5fa28aa9bfb65b150bd612e5a450091
Wasseem Khayrattee <w@7php.com>
2022-06-29 00:40:49 +0400

    refactor: change custom default values to reflect a custom example + adjust licensing version we are issuing for this repo

--- plugin-env.sh
+++ plugin-env.sh
@@ -3,6 +3,7 @@
 ### A central place to add all reusable stuffs
 ### @author Wasseem Khayrattee <github@wk.contact>
 ### @github wkhayrattee
+### @license GPL-3.0-only
 ##
 
 AUTHOR_NAME="Wasseem Khayrattee"
@@ -10,14 +11,14 @@ AUTHOR_WP_ORG_USERNAME="wkhayrattee"
 AUTHOR_EMAIL="github@wk.contact"
 AUTHOR_URI="https://github.com/wkhayrattee"
 ### should be in a valid slug
-PROJECT_SLUG="wp-plugin-starter"
-PROJECT_NAME="WP Plugin Starter"
+PROJECT_SLUG="my-custom-plugin-name"
+PROJECT_NAME="My Custom Plugin Name"
 PROJECT_GITHUB="todo"
 #
 COMPOSER_NAMESPACE="wkhayrattee"
 ## Should be CamelCase
-PSR_AUTOLOAD_NAMESPACE="WpPluginStarter"
-PHP_CONSTANT_PREFIX="WP_PLUGIN_STARTER"
+PSR_AUTOLOAD_NAMESPACE="MyCustomPlugin"
+PHP_CONSTANT_PREFIX="MY_CUSTOM_PLUGIN"
 ## to house handy procedural functions
 HELPER_FILE_NAME="${PROJECT_SLUG}-plugin-helper"
 #
