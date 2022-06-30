80599e345e62606c7cdfdd4b9b9dd9e2c5fe0d7f
Wasseem Khayrattee <w@7php.com>
2022-06-30 19:42:56 +0400

    refactor: change precedence of env files call

--- bin/header.sh
+++ bin/header.sh
@@ -11,8 +11,8 @@ export DEBIAN_FRONTEND=noninteractive
 BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ROOT_DIR="$( cd "$( dirname "$SCRIPT_DIR" )" >/dev/null 2>&1 && pwd )"
 
-source $BIN_DIR/env.sh
 source $ROOT_DIR/plugin-env.sh
+source $BIN_DIR/env.sh
 
 ## Change directory
 ## Probably safer than assuming `cd ..`, let's be predictable
