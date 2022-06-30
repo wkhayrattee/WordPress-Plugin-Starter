ff39984ce9df06419d1df7355a56de204cd8e213
Wasseem Khayrattee <w@7php.com>
2022-06-30 21:33:43 +0400

    feat(sed command): add support on both OSX and Linux.
    The sed command has different syntx on Linux (gnu sed) vs the one on Mac.

--- bin/header.sh
+++ bin/header.sh
@@ -4,16 +4,27 @@
 ### @github wkhayrattee
 ##
 export DEBIAN_FRONTEND=noninteractive
-##
-### get current dir path for our main git repo
-### this is one-step before folder `bin`
-##
+#
+## To add `sed` support on both OSX and Linux
+## ref: https://stackoverflow.com/a/66763713
+#
+PORTABLE_SED_OPTION=
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  PORTABLE_SED_OPTION="-i \x27\x27"
+else
+  PORTABLE_SED_OPTION='-i';
+fi;
+#
+## get current dir path for our main git repo
+## this is one-step before folder `bin`
+#
 BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ROOT_DIR="$( cd "$( dirname "$SCRIPT_DIR" )" >/dev/null 2>&1 && pwd )"
-
+#
 source $ROOT_DIR/plugin-env.sh
 source $BIN_DIR/env.sh
-
+#
 ## Change directory
 ## Probably safer than assuming `cd ..`, let's be predictable
+#
 cd $ROOT_DIR
