a9fb49ceb4564c1c6c2dc8ec51a633edad2af0b9
Wasseem K. (Ringier SA) <wasseemk@ringier.co.za>
2022-07-04 01:13:05 +0400

    refactor(template): Refactor all Admin related logic into a separate admin class, namely Admin/Dashboard.php

--- template/src/Core/PluginManager.php
+++ template/src/Core/PluginManager.php
@@ -2,6 +2,8 @@
 
 namespace %PSR4_NAMESPACE%;
 
+use %PSR4_NAMESPACE%\Admin\Dashboard;
+
 class PluginManager
 {
     /**
@@ -56,53 +58,10 @@ class PluginManager
             delete_option(Enum::PLUGIN_KEY);
         }
 
-        //register hooks
-        add_action('admin_init', [self::class, 'admin_init_callback']);
-        add_action('admin_menu', [self::class, 'admin_menu_callback']);
-        add_action('admin_enqueue_scripts', [self::class, 'load_resources_callback']);
-    }
-
-    /**
-     * Fires as an admin screen or script is being initialized.
-     * Is triggered before any other hook when a user accesses the admin area
-     *
-     * Runs BOTH on user-facing admin screens and on admin-ajax.php & admin-post.php as well
-     * Roughly analogous to the more general ‘init’ hook, which fires earlier.
-     *
-     * ref: https://developer.wordpress.org/reference/hooks/admin_init/
-     */
-    public static function admin_init_callback()
-    {
-
-    }
-
-    /**
-     * To add the plugin's submenus & menu options to the admin panel’s menu structure
-     *
-     * Fires before the administration menu loads in the admin.
-     * Runs after the basic admin panel menu structure is in place.
-     *
-     * Note:
-     * Must not be placed in an admin_init action function because the admin_init action is called after admin_menu.
-     *
-     * ref: https://developer.wordpress.org/reference/hooks/admin_menu/
-     */
-    public static function admin_menu_callback()
-    {
-
-    }
-
-    /**
-     * To load assets files (e.g: css & js) for Admins creen
-     */
-    public static function load_resources_callback()
-    {
-        $screen = get_current_screen();
-
-        // load the asset only when the admin page is on the slug of our plugin
-        if ('%PROJECT_SLUG%' === $screen->base) {
-            wp_enqueue_style('%PROJECT_SLUG%-css', %PHP_CONSTANT_PREFIX%_PLUGIN_DIR_URL . 'assets/css/%PROJECT_SLUG%.css', %PHP_CONSTANT_PREFIX%_PLUGIN_VERSION);
-            wp_enqueue_script('%PROJECT_SLUG%-js', %PHP_CONSTANT_PREFIX%_PLUGIN_DIR_URL . 'assets/js/%PROJECT_SLUG%.js', ['jquery'], %PHP_CONSTANT_PREFIX%_PLUGIN_VERSION);
+        if (!current_user_can('manage_options')) {
+            return;
         }
+
+        new Dashboard();
     }
 }
