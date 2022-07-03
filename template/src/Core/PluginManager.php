b0af72d329f13f5f4159b046fbef7739feb41b7e
Wasseem Khayrattee <w@7php.com>
2022-07-04 00:06:56 +0400

    refactor(template): revamp call for admin_init, admin_menu hooks, resouce loading and add method for frontend logic

--- template/src/Core/PluginManager.php
+++ template/src/Core/PluginManager.php
@@ -36,6 +36,14 @@ class PluginManager
     }
 
     /**
+     * To call any logic that needs to run on the frontend or in background
+     */
+    public static function run()
+    {
+
+    }
+
+    /**
      * Render the admin pages
      */
     public static function adminInit()
@@ -48,22 +56,53 @@ class PluginManager
             delete_option(Enum::PLUGIN_KEY);
         }
 
-        //Now do normal stuff
-        add_action('admin_menu', [self::class, 'handleAdminUI']);
+        //register hooks
+        add_action('admin_init', [self::class, 'admin_init_callback']);
+        add_action('admin_menu', [self::class, 'admin_menu_callback']);
+        add_action('admin_enqueue_scripts', [self::class, 'load_resources_callback']);
     }
 
-    public static function handleAdminUI()
+    /**
+     * Fires as an admin screen or script is being initialized.
+     * Is triggered before any other hook when a user accesses the admin area
+     *
+     * Runs BOTH on user-facing admin screens and on admin-ajax.php & admin-post.php as well
+     * Roughly analogous to the more general ‘init’ hook, which fires earlier.
+     *
+     * ref: https://developer.wordpress.org/reference/hooks/admin_init/
+     */
+    public static function admin_init_callback()
     {
-        add_action('admin_enqueue_scripts', [self::class, 'add_javascript']);
+
     }
 
-    public static function add_javascript()
+    /**
+     * To add the plugin's submenus & menu options to the admin panel’s menu structure
+     *
+     * Fires before the administration menu loads in the admin.
+     * Runs after the basic admin panel menu structure is in place.
+     *
+     * Note:
+     * Must not be placed in an admin_init action function because the admin_init action is called after admin_menu.
+     *
+     * ref: https://developer.wordpress.org/reference/hooks/admin_menu/
+     */
+    public static function admin_menu_callback()
+    {
+
+    }
+
+    /**
+     * To load assets files (e.g: css & js) for Admins creen
+     */
+    public static function load_resources_callback()
     {
         $screen = get_current_screen();
 
         // load the asset only when the admin page is on the slug of our plugin
         if ('%PROJECT_SLUG%' === $screen->base) {
-            wp_enqueue_script('%PROJECT_SLUG%-js', %PHP_CONSTANT_PREFIX%_PLUGIN_DIR_URL . 'assets/js/%PROJECT_SLUG%-script.js', ['jquery'], '%PHP_CONSTANT_PREFIX%_PLUGIN_VERSION');
+            wp_enqueue_style('%PROJECT_SLUG%-css', %PHP_CONSTANT_PREFIX%_PLUGIN_DIR_URL . 'assets/css/%PROJECT_SLUG%.css', %PHP_CONSTANT_PREFIX%_PLUGIN_VERSION);
+            wp_enqueue_script('%PROJECT_SLUG%-js', %PHP_CONSTANT_PREFIX%_PLUGIN_DIR_URL . 'assets/js/%PROJECT_SLUG%.js', ['jquery'], %PHP_CONSTANT_PREFIX%_PLUGIN_VERSION);
         }
     }
 }
