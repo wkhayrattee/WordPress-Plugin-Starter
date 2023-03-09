1a8acdb18eba80e352046a12e6629da9350168fd
Wasseem Khayrattee <wasseemk@ringier.co.za>
2023-03-09 15:36:38 +0400

    feat: some more enums related to the log page..etc

--- template/src/Core/Enum.php
+++ template/src/Core/Enum.php
@@ -5,4 +5,10 @@ namespace %PSR4_NAMESPACE%;
 class Enum
 {
     public const PLUGIN_KEY = '%PROJECT_SLUG%';
+
+    const ADMIN_PAGE_CAPABILITY = 'manage_options';
+    const ADMIN_PAGE_OPTION_GROUP = '%PROJECT_SLUG%_admin_option_group';
+    const ADMIN_PAGE_OPTION_NAME = '%PROJECT_SLUG%_admin_option_name';
+    const ADMIN_PAGE_PARENT_SLUG = '%PROJECT_SLUG%-admin-home';
+    const ADMIN_PAGE_LOG_SLUG = '%PROJECT_SLUG%-admin-log';
 }
