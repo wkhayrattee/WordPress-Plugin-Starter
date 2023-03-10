7c855469e32e7d8f64ec5e0d73b879c79861d440
Wasseem Khayrattee <wasseemk@ringier.co.za>
2023-03-10 16:00:07 +0400

    feat: add new template file for core Utils class

---
+++ template/src/Core/Utils.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace %PSR4_NAMESPACE%;
+
+class Utils
+{
+    /**
+     * @param $level
+     * @param $message
+     * @param array $context
+     */
+    public static function log($level, $message, array $context = []): void
+    {
+        error_log(PHP_EOL . date('F j, Y, g:i a e O') . ' | ' . mb_strtoupper($level) . ': ' . $message . ' ' .
+            (!empty($context) ? print_r($context, true) : ''), 3, %PHP_CONSTANT_PREFIX%_PLUGIN_ERROR_LOG_FILE);
+    }
+}
